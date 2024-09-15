787a1c07e3919fc4a27023df90d20f0a613eae93
ipi <stnrfzhlfnksm21@gmail.com>
2024-09-15 16:44:53 +0800

    fix redirect

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
 
         $request->session()->regenerate();
 
-        return $this->redirect('/', 'success', 'Login Berhasil');
+        return redirect()->intended('/');
     }
 
     public function logout(Request $request): RedirectResponse

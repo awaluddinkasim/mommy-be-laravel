e5b9a63f5e3eb217bd5bc36821df59a67cd99ec6
ipi <stnrfzhlfnksm21@gmail.com>
2024-09-15 16:42:20 +0800

    auth controllers

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -3,9 +3,41 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class AuthController extends Controller
 {
-    //
+    public function login(): View
+    {
+        return view('auth');
+    }
+
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!auth('admin')->attempt($request->only('email', 'password'))) {
+            return $this->back('error', 'Email atau Password salah!');
+        }
+
+        $request->session()->regenerate();
+
+        return $this->redirect('/', 'success', 'Login Berhasil');
+    }
+
+    public function logout(Request $request): RedirectResponse
+    {
+        $request->session()->invalidate();
+
+        $request->session()->regenerateToken();
+
+        auth('admin')->logout();
+
+        return $this->redirect('/', 'success', 'Logout Berhasil');
+    }
 }
--- app/Http/Controllers/User/AuthController.php
+++ app/Http/Controllers/User/AuthController.php
@@ -3,9 +3,35 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    //
+    public function login(Request $request): JsonResponse
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (!auth('api')->attempt($request->only('email', 'password'))) {
+            return $this->error('Email atau Password salah!');
+        }
+
+        $user = User::find(auth('api')->user()->id);
+
+        return $this->success([
+            'user' => $user,
+            'token' => $user->createToken('auth-token')->plainTextToken
+        ]);
+    }
+
+    public function logout(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return $this->success(null, 'Logout Berhasil');
+    }
 }

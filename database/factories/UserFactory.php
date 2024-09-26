dce9f26f2fa575af4a9feca00107cde2a47df4ab
Lab Komputer 1 <labkomputer1@uim-makassar.ac.id>
2024-09-26 12:05:33 +0800

    user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -24,11 +24,12 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
+            'nama' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'tgl_lahir' => fake()->date(),
+            'no_hp' => fake()->phoneNumber(),
         ];
     }
 
@@ -37,7 +38,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
+        return $this->state(fn(array $attributes) => [
             'email_verified_at' => null,
         ]);
     }

1ebd6d2c98692c41ca4a63671d7ee09c953642e1
Awaluddin Kasim <awaluddinkasim7@gmail.com>
2024-10-30 09:10:22 +0800

    update controller

--- app/Http/Controllers/Admin/NotifikasiController.php
+++ app/Http/Controllers/Admin/NotifikasiController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Notification;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class NotifikasiController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('pages.notifikasi', [
+            'notifications' => Notification::all(),
+        ]);
+    }
+
+    public function destroy(Notification $notification): RedirectResponse
+    {
+        $notification->delete();
+        return redirect()->back();
+    }
 }
--- app/Http/Controllers/Admin/UserController.php
+++ app/Http/Controllers/Admin/UserController.php
@@ -3,9 +3,50 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class UserController extends Controller
 {
-    //
+    public function index(): View
+    {
+        return view('pages.pengguna', [
+            'users' => User::all(),
+        ]);
+    }
+
+    public function edit(User $user): View
+    {
+        return view('pages.pengguna-edit', [
+            'user' => $user,
+        ]);
+    }
+
+    public function update(Request $request, User $user)
+    {
+        $data = $request->validate([
+            'nama' => 'required',
+            'email' => 'required',
+            'password' => 'nullable|min:8',
+            'tgl_lahir' => 'required',
+            'no_hp' => 'required',
+        ]);
+
+        if ($data['password'] !== null) {
+            $data['password'] = bcrypt($data['password']);
+        } else {
+            unset($data['password']);
+        }
+
+        $user->update($data);
+
+        return redirect()->route('pengguna');
+    }
+
+    public function destroy(User $user)
+    {
+        $user->delete();
+        return redirect()->route('pengguna');
+    }
 }
